312f8fdde81019e031d6eeb2ff2ff11839ca259e
hannahschwen <hannahschwen@gmail.com>
2025-04-04 21:29:02 -0400

    mine rocks

--- scenes/Objects/Rocks/rock.gd
+++ scenes/Objects/Rocks/rock.gd
@@ -2,7 +2,7 @@ extends Sprite2D
 @onready var hurt_component: HurtComponent = $HurtComponent
 @onready var damage_component: DamageComponent = $DamageComponent
 
-var log_scene = preload("res://scenes/Objects/Trees/log.tscn")
+var stone_scene = preload("res://scenes/Objects/Rocks/stone.tscn")
 
 func _ready() -> void:
 	hurt_component.hurt.connect(on_hurt)
@@ -10,15 +10,15 @@ func _ready() -> void:
 	
 func on_hurt(hit_damage: int) -> void:
 	damage_component.apply_damage(hit_damage)
-	material.set_shader_parameter("shake_intensity", 0.5)
-	await get_tree().create_timer(1.0).timeout
+	material.set_shader_parameter("shake_intensity", 0.3)
+	await get_tree().create_timer(0.5).timeout
 	material.set_shader_parameter("shake_intensity", 0.0)
 	
 func on_max_damage_reached() -> void:
-	call_deferred("add_log_scene")
+	call_deferred("add_stone_scene")
 	queue_free()
 
-func add_log_scene() -> void:
-	var log_instance = log_scene.instantiate() as Node2D
-	log_instance.global_position = global_position
-	get_parent().add_child(log_instance)
+func add_stone_scene() -> void:
+	var stone_instance = stone_scene.instantiate() as Node2D
+	stone_instance.global_position = global_position
+	get_parent().add_child(stone_instance)
